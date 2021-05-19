88cf790496129b2ec5fa7a4910bf32994dfc79db
mgbams <cmgbams@gmail.com>
2021-05-19 17:35:36 +0200

    play, pause and previous music functionality added

--- js/script.js
+++ js/script.js
@@ -33,3 +33,29 @@ function playMusic() {
   playPauseBtn.querySelector("i").innerText = "pause";
   mainAudio.play();
 }
+//pause music function
+function pauseMusic() {
+  wrapper.classList.remove("paused");
+  playPauseBtn.querySelector("i").innerText = "play_arrow";
+  mainAudio.pause();
+}
+
+//prev music function
+function prevMusic() {
+  musicIndex--; //decrement of musicIndex by 1
+  //if musicIndex is less than 1 then musicIndex will be the array length so the last music play
+  musicIndex < 1 ? (musicIndex = allMusic.length) : (musicIndex = musicIndex);
+  loadMusic(musicIndex);
+  playMusic();
+  playingSong();
+}
+
+//next music function
+function nextMusic() {
+  musicIndex++; //increment of musicIndex by 1
+  //if musicIndex is greater than array length then musicIndex will be 1 so the first music play
+  musicIndex > allMusic.length ? (musicIndex = 1) : (musicIndex = musicIndex);
+  loadMusic(musicIndex);
+  playMusic();
+  playingSong();
+}
